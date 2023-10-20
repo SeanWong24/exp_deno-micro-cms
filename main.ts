c46d00b306bf85c872f613c96f859ac23b24509a
Sean Wong <seanwong24@live.com>
2023-10-20 17:09:11 +0000

    update main.ts

--- main.ts
+++ main.ts
@@ -3,19 +3,39 @@
 import {
   App,
   Area,
+  Body,
   Controller,
   CorsBuilder,
   Get,
+  Post,
+  QueryParam,
 } from "https://deno.land/x/alosaur@v0.33.0/mod.ts";
 
-const kv = await Deno.openKv();
-await kv.set(["main", "test"], "Hello World!");
+const KV = await Deno.openKv();
+const STRING_BODY_TRANSFORMER = async (body: ReadableStream) =>
+  await new Response(body).text();
 
-@Controller()
+@Controller("")
 export class HomeController {
   @Get()
-  async text() {
-    return (await kv.get(["main", "test"])).value;
+  getValue(@QueryParam("key") key: string) {
+    if (!key) {
+      return;
+    }
+    const keyArray = key.split(".");
+    return KV.get(keyArray);
+  }
+
+  @Post()
+  setValue(
+    @QueryParam("key") key: string,
+    @Body(STRING_BODY_TRANSFORMER) value: string,
+  ) {
+    if (!key) {
+      return;
+    }
+    const keyArray = key.split(".");
+    return KV.set(keyArray, value);
   }
 }
 
