69364d89b520c0ae8c99f2cb84db110355d176f8
Sean Wong <seanwong24@live.com>
2023-11-01 20:58:09 +0000

    modify default dev mode db path

--- utils/app-config.ts
+++ utils/app-config.ts
@@ -16,5 +16,6 @@ export const APP_CONFIG: AppConfig = {
   FE_INDEX_PATH: Deno.env.get("FE_INDEX_PATH") || "index.html",
   ADMIN_ROOT_PATH: Deno.env.get("ADMIN_ROOT_PATH") || `${Deno.cwd()}/www_admin`,
   ADMIN_INDEX_PATH: Deno.env.get("ADMIN_INDEX_PATH") || "index.html",
-  DB_PATH: Deno.env.get("DB_PATH"),
+  DB_PATH: Deno.env.get("DB_PATH") ||
+    (Deno.env.get("DEV") ? `${Deno.cwd()}/temp/db` : ""),
 };
