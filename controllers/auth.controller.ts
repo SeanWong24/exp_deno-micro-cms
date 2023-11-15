65ba93594c5b41b10240bb89a59c77b5e533a14f
Sean Wong <seanwong24@live.com>
2023-11-15 05:45:18 +0000

    add domain prop for cookies

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,8 +1,13 @@
 import {
+  AlosaurRequest,
+  AlosaurResponse,
   Controller,
+  Ctx,
   Get,
+  HttpContext,
   Post,
   QueryParam,
+  Req,
   Res,
   UseHook,
 } from "../deps/alosaur.ts";
@@ -20,13 +25,16 @@ export class AuthController {
   }
 
   @Post("/sign-in")
-  signIn(@QueryParam("passcode") passcode: string, @Res() response: Response) {
+  signIn(@QueryParam("passcode") passcode: string, @Res() response: AlosaurResponse, @Req() request: AlosaurRequest) {
     if (APP_CONFIG.passcode !== passcode) {
       throw new NotAuthenticatedError("Invalid passcode.");
     }
+    const host = request.headers.get('X-Forwarded-Host') ?? request.headers.get('Host');
+    const domain = host ? new URL(`http://${host}`).hostname : undefined;
     setCookie(response.headers, {
       name: "authenticated",
       value: "1",
+      domain,
       path: "/",
       httpOnly: true,
       sameSite: APP_CONFIG.cors ? 'None' : 'Lax',
@@ -36,8 +44,11 @@ export class AuthController {
   }
 
   @Post("/sign-out")
-  signOut(@Res() response: Response) {
+  signOut(@Res() response: AlosaurResponse, @Req() request: AlosaurRequest) {
+    const host = request.headers.get('X-Forwarded-Host') ?? request.headers.get('Host');
+    const domain = host ? new URL(`http://${host}`).hostname : undefined;
     deleteCookie(response.headers, "authenticated", {
+      domain,
       path: "/",
       sameSite: APP_CONFIG.cors ? 'None' : 'Lax',
       secure: APP_CONFIG.cors ? true : undefined
