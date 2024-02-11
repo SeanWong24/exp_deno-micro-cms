1eaf57536d7f95302aa63fa72731a528f799fdeb
Sean Wong <seanwong24@live.com>
2024-02-11 21:23:33 +0000

    export startApp function

--- mod.ts
+++ mod.ts
@@ -1,13 +1,19 @@
 import { Application } from "oak";
 import { oakCors } from "cors";
-import config from "./service/config.ts";
+import config, { AppConfig } from "./service/config.ts";
 import router from "./router.ts";
 
-const app = new Application();
+export async function startApp(config: AppConfig = {}) {
+  const app = new Application();
 
-if (config.CORS) {
-  app.use(oakCors({ origin: new RegExp(config.CORS) }));
+  if (config.CORS) {
+    app.use(oakCors({ origin: new RegExp(config.CORS) }));
+  }
+  app.use(router.routes());
+
+  await app.listen({ port: config.PORT ? +config.PORT : 8000 });
 }
-app.use(router.routes());
 
-await app.listen({ port: +config.PORT });
+if (import.meta.main) {
+  await startApp(config);
+}
--- service/config.ts
+++ service/config.ts
@@ -1,10 +1,14 @@
-const config = {
+type AppConfigKeys = 'DB_PATH' | 'CORS' | 'BLOB_PATH' | 'PASSCODE' | 'ADMIN_UI' | 'PORT';
+
+export type AppConfig = Partial<Record<AppConfigKeys, string>>;
+
+const config: AppConfig = {
   DB_PATH: Deno.env.get("DB_PATH"),
   CORS: Deno.env.get("CORS"),
   BLOB_PATH: Deno.env.get("BLOB_PATH"),
   PASSCODE: Deno.env.get("PASSCODE"),
   ADMIN_UI: Deno.env.get("ADMIN_UI"),
-  PORT: Deno.env.get('PORT') ?? '8000'
+  PORT: Deno.env.get('PORT')
 };
 
 export default config;
